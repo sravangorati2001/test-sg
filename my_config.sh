bf5901635f6238c52e664d6c6c4336afd22dea17
sravangorati2001 <sravangorati2001@gmail.com>
2024-05-19 13:24:28 -0700

    yml changes

--- my_config.sh
+++ my_config.sh
@@ -1,6 +1,6 @@
-export SG_ROLE= #An AWS IAM Role with Full SG Access (I give full S3 Access too fwiw )
-export WORK_DIRECTORY= #the name of your local data_dir, i.e., data
-export PREFIX= #the prefix you want for you S3 bucket e.g., DEMO-scikit-iris-gus
-export FRAMEWORK_VERSION=1.0-1
+export SG_ROLE=arn:aws:iam::992382664147:role/awssagemaker #An AWS IAM Role with Full SG Access (I give full S3 Access too fwiw )
+export WORK_DIRECTORY=data #the name of your local data_dir, i.e., data
+export PREFIX="DEMO-scikit-iris-gus" #the prefix you want for you S3 bucket e.g., DEMO-scikit-iris-gus
+export FRAMEWORK_VERSION="1.0-1"
 export SCRIPT_PATH=train.py
-export INSTANCE_TYPE= #the instance size you want to use, i.e., ml.m5.large
+export INSTANCE_TYPE="ml.m5.large" #the instance size you want to use, i.e., ml.m5.large
