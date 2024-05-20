76a1a72034483d10702ca6c06763954e34af0aa7
sravangorati2001 <sravangorati2001@gmail.com>
2024-05-19 22:05:31 -0700

    changes

--- lambda_function.py
+++ lambda_function.py
@@ -1,36 +1,19 @@
 import json
-import joblib
-import boto3
-import os
-
-s3 = boto3.client('s3')
-s3_bucket = 'traindata-pyxer1'
-s3_key_model = 'model/model.joblib'
-s3_key_encoder = 'model/label_encoder.joblib'
-model_path = '/tmp/model.joblib'
-encoder_path = '/tmp/label_encoder.joblib'
+import pickle
+import pandas as pd
 
 def lambda_handler(event, context):
-    # Download model and label encoder from S3
-    s3.download_file(s3_bucket, s3_key_model, model_path)
-    s3.download_file(s3_bucket, s3_key_encoder, encoder_path)
+    # Load the model
+    with open('/tmp/model.pkl', 'rb') as f:
+        model = pickle.load(f)
     
-    model = joblib.load(model_path)
-    label_encoder = joblib.load(encoder_path)
+    # Process the input data (assuming it's passed in the event)
+    input_data = pd.DataFrame(event['data'])
     
-    # Parse input data
-    input_data = json.loads(event['body'])['data']
-
-    # Predict
-    prediction = model.predict([input_data])
-    prediction_label = label_encoder.inverse_transform(prediction)
+    # Make predictions
+    predictions = model.predict(input_data)
     
     return {
         'statusCode': 200,
-        'body': json.dumps({
-            'prediction': prediction_label.tolist()
-        }),
-        'headers': {
-            'Content-Type': 'application/json'
-        }
+        'body': json.dumps(predictions.tolist())
     }
--- train.py
+++ train.py
@@ -1,24 +1,18 @@
-import joblib
 import pandas as pd
-from sklearn import tree
-from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+import pickle
 
-def train():
-    # Load dataset
-    data = pd.read_csv('data/iris.csv', header=None)
-    X, y = data.iloc[:, :-1], data.iloc[:, -1]
-    
-    # Encode labels
-    label_encoder = LabelEncoder()
-    y = label_encoder.fit_transform(y)
+# Load the dataset
+data = pd.read_csv('data/iris.csv')
 
-    # Train model
-    clf = tree.DecisionTreeClassifier(max_leaf_nodes=30)
-    clf.fit(X, y)
+# Assume the dataset has features in columns 0 to -1 and target in the last column
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
 
-    # Save model and label encoder
-    joblib.dump(clf, 'model.joblib')
-    joblib.dump(label_encoder, 'label_encoder.joblib')
+# Train the model
+model = LogisticRegression()
+model.fit(X, y)
 
-if __name__ == "__main__":
-    train()
+# Save the model
+with open('model.pkl', 'wb') as f:
+    pickle.dump(model, f)
