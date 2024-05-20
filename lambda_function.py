63d19b372189e21398df8fcc8586d1e8784d25f0
sravangorati2001 <sravangorati2001@gmail.com>
2024-05-20 01:50:07 -0700

    changes

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,15 @@
 import json
 import pickle
+import boto3
 import pandas as pd
 
 def lambda_handler(event, context):
+    # Download the model from S3 to /tmp/model.pkl
+    s3 = boto3.client('s3')
+    s3_bucket = 'traindata-pyxer1'
+    s3_key = 'model.pkl'
+    s3.download_file(s3_bucket, s3_key, '/tmp/model.pkl')
+    
     # Load the model
     with open('/tmp/model.pkl', 'rb') as f:
         model = pickle.load(f)
