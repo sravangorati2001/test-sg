733bb887c31846c7097a2aafbbe4ea4e50de93c3
sravangorati2001 <sravangorati2001@gmail.com>
2024-05-20 02:01:27 -0700

    changes

--- train.py
+++ train.py
@@ -2,14 +2,17 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 import pickle
 
+# Load the training data
 data = pd.read_csv('data/iris.csv')
 
+# Separate features and target
+X = data[['feature1', 'feature2']]
+y = data['target']
 
-X = data.iloc[:, :-1]
-y = data.iloc[:, -1]
-
+# Train the model
 model = LogisticRegression()
 model.fit(X, y)
 
+# Save the model to a file
 with open('model.pkl', 'wb') as f:
     pickle.dump(model, f)
